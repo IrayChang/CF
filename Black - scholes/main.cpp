0b864af04d81536d4cea199cc980efe9d77d4c56
IrayChang <feng-jui.chang@outlook.com>
2025-01-30 22:33:27 +0000

    Initial Commit

---
+++ Black - scholes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Black - scholes
+//
+//  Created by 張峰瑞 on 30/01/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
